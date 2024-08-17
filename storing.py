3dc87c008c2385fd9e0402ba994d36af8f8a5bf4
viciouis <aryaen.sharma21@gmail.com>
2024-08-16 23:06:46 -0400

    Storage donezo

--- storing.py
+++ storing.py
@@ -1,11 +1,33 @@
 import os
-if os.path.exists('storage.txt'): #delete file if it alredy exists so its blank
-    os.remove('storage.txt')
-myfile = open('storage.txt','x')#create file
-myfile = open('storage.txt','a')#opens file for appending
+from main import *
+lastelement=False
+count=0
 
-mylist = ['ex1','ex2','ex3','ex4'] #add the actual data later
-for i in mylist:
-    myfile.write(i)
-    myfile.write('\n')
-    #pass
+if os.path.exists('storage.json'): #delete file if it alredy exists so its blank
+    os.remove('storage.json')
+StorageFile = open('storage.json','x')#create file
+StorageFile = open('storage.json','a')#opens file for appending
+
+InputString=myBook
+StorageList=[]
+word=str('')
+for i in InputString:
+    if count==len(InputString)-1:
+        lastelement=True
+    if lastelement==True:
+        word+=i
+        StorageList.append(word)
+        break
+    if str(i)!=' ':
+        word+=i
+    else:
+        StorageList.append(word)
+        word=''
+    count+=1
+    
+
+
+#StorageList = ['ex1','ex2','ex3','ex4'] #add the actual data later
+for i in StorageList:
+    StorageFile.write(i)
+    StorageFile.write('\n')
