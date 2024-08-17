f1df1ff14ad59286d3188757bb46eeabedb3e0d2
Harsharan Rakhra <HarsharanRakhra@outlook.com>
2024-08-17 12:30:53 -0400

    tts code completed

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import cv2
 import easyocr
 import matplotlib.pyplot as plt
 
-import os
-from gtts import gTTS
-import playsound
-
+from pygame import mixer 
+import pyttsx3
 import time
+import wave
 
+
+#init and setup TTS libraries
+mixer.init() 
+speaker = pyttsx3.init()
+speaker.setProperty('rate', 150)  # Speed of speech (words per minute)
+speaker.setProperty('volume', 1.0)  # Volume (0.0 to 1.0)
+
+#helper function for TTS
+def get_duration_wave(file_path):
+   with wave.open(file_path, 'r') as audio_file:
+      frame_rate = audio_file.getframerate()
+      n_frames = audio_file.getnframes()
+      duration = n_frames / float(frame_rate)
+      return duration
+
+#TTS generation function
 def speak(text):
-    if os.path.exists('voice.mp3'): #delete file if it alredy exists so its blank
-        os.remove('voice.mp3')
-    tts = gTTS(text=text, lang="en")
-    filename = "voice.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
+    speaker.save_to_file(text, "output.wav")
+    speaker.runAndWait()
+    mixer.music.load("output.wav") 
+    
+#testing tts
+# note the pygame command is nonobstructing so there must be code to continously run the program for the audio to run  
+speak("hello this is a test")
+mixer.music.play() 
+time.sleep(get_duration_wave("output.wav"))
 
 
- 
 
 #read image 
 image_path = 'C:/Users/Harsh/OneDrive/Documents/Projects/Hackathon/Ignition Hacks/ReadAble/Images/test3.jpg'
